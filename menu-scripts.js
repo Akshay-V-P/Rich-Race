ce56711d85925aa27b283df30ef527135f7efab6
Akshay VP <akshayvpcontact@gmail.com>
2024-09-30 14:09:09 +0530

    RealEstate route setted

--- menu-scripts.js
+++ menu-scripts.js
@@ -2,6 +2,7 @@ import { setInitialValues } from "./initialize-values.js"
 
 const howToPlayDiv = document.querySelector(".how-to-play")
 const restartbtn = document.querySelector(".restart-game") // restart button
+const realEstateBtn = document.querySelector('.house-purchase')
 
 howToPlayDiv.addEventListener('click', ()=>{
     window.location.href = "how-to-play.html"
@@ -11,4 +12,8 @@ restartbtn.addEventListener("click", ()=>{
     setInitialValues()
     localStorage.removeItem("professionSelected")
     window.location.href = "select-profession.html"
+})
+
+realEstateBtn.addEventListener('click', ()=>{
+    window.location.href = "house-purchase.html"
 })
